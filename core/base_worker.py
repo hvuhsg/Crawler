5d41c7b8ed716fd24357f68a7f31b0c3ee2afb91
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2020-09-03 15:36:48 +0300

    Add doc-strings to functions

--- core/base_worker.py
+++ core/base_worker.py
@@ -1,5 +1,6 @@
 from threading import Thread
-from abc import ABC, abstractmethod
+from abc import abstractmethod
+from typing import Tuple, List
 
 
 class BaseWorker:
@@ -9,28 +10,51 @@ class BaseWorker:
         self._thread = Thread(target=self.loop)
 
     def run(self):
+        """
+        Run the worker thread
+        :return:
+        """
         self._stop = False
         self._thread.start()
 
     def stop(self):
+        """
+        Stop the worker thread
+        :return:
+        """
         self._stop = True
 
     def loop(self):
+        """
+        While the worker run's call round method
+        :return:
+        """
         while not self._stop:
             self.round()
 
     def round(self):
+        """
+        Get link, find all it's sub links and save them
+        """
         link, link_depth = self.storage.get_link()
-        if link_depth is None:
+        if link is None:
             self._stop = True
             return
-        sublinks = self.find_sublinks(link)
+        sublinks, link_data = self.find_sublinks(link)
+        self.process_link_data(link_data)
         self.storage.push_links(sublinks, link_depth + 1, link)
 
+    def process_link_data(self, link_data):
+        """
+        Do what ever you want with the data from the link (search for emails in the html page for example)
+        :param link_data: the data from the link
+        """
+        return NotImplemented
+
     @abstractmethod
-    def find_sublinks(self, link: str) -> list:
+    def find_sublinks(self, link: str) -> Tuple[List[str], object]:
         """
         :param link: <str> link to web page
-        :return: list of links<str>
+        :return: (list of links<str>, link_data)
         """
         raise NotImplemented
