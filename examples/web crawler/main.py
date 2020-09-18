53bc0a8bd32f66829f153d81309b69ff6ff9f5a1
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2020-09-18 14:50:04 +0300

    Update example (to use sqlite3)

--- examples/web crawler/main.py
+++ examples/web crawler/main.py
@@ -1,18 +1,14 @@
-from Crawler.StorageTypes.mongo_storage import Storage
+from Crawler.storage_types.sqlite_storage import Storage
 from Crawler.crawler import Crawler
 from worker import Worker
 
 
 def main():
-    base_url = 'kiryat4.org.il'
-    depth = 5
+    base_url = 'en.wikipedia.org/wiki/Main_Page'
+    depth = 2
 
-    mongo_storage = Storage(base_url=base_url,
-                            db_name='crawlerDB',
-                            username='crawler_username',
-                            password="crawler_password",
-                            max_depth=depth)
-    crawler = Crawler(base_url=base_url, depth=depth, storage=mongo_storage, worker_class=Worker, workers_number=5)
+    sqlite_storage = Storage(db_name="storage.db", base_url=base_url, max_depth=depth)
+    crawler = Crawler(base_url=base_url, depth=depth, storage=sqlite_storage, worker_class=Worker, workers_number=2)
     crawler.create_workers()
     crawler.run_workers()
     crawler.idle()
